c51b745b21d9f7088a0a38080325f0ed61955b9f
Colby Frey <colby.frey@rocklin.k12.ca.us>
2020-01-14 15:39:25 -0800

    Inital commit

---
+++ V1.py
@@ -0,0 +1,34 @@
+import time
+import sys
+
+
+
+print('Hello Welcome to the CTF Swiss Army Knife Toolkit! ')
+print('Created by Colby Frey\n')
+time.sleep(1)
+print('Please select your desired tool \n')
+
+
+def selectionScreen():
+	print('1. John Pollard n-1')
+	print('2. Rot-13')
+	print('3. Exit')
+
+	tool = raw_input('\n')
+
+	if tool == '1':
+		import JohnPollard
+	if tool == '2':
+		print('not done yet')
+	if tool == '3':
+		sys.exit(0)
+
+	else:
+		print('Invalid Selection, Try again')
+		selectionScreen()
+
+
+
+
+
+selectionScreen()
