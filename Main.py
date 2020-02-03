f3b4127b278a6632089d16f4326e4495151f43b6
Colby <55164236+RT-Colby@users.noreply.github.com>
2020-02-03 10:28:53 -0800

    2/2

--- Main.py
+++ Main.py
@@ -1,38 +1,61 @@
 from tkinter import *
+import tkinter.ttk as tkrtk
 import sys
 import os
 from JohnPollard import *
+
+#Initalize intial values
+labelsList = []
+buttonsList = []
+entriesList = []
+
 
 #Create window
 master = Tk()
 master.title('CTF SAK-Toolkit')
 master.geometry('500x300')
 
-#button method
+
+#Methods
 def buttonClick():
 	text = pollard_P_1(int(jpEntry.get()))
 	pLabel = Label(master, text=text)
 	pLabel.grid(row = 5)
-
-
+def restWindow():
+	for label in labelsList:
+		labelsList.destory()
+	for button in buttonsList:
+		buttonsList.destory()
+	for Entry in entriesList:
+		entriesList.destory()
 
 
 #Intro labels
-welcome_label = Label(master, text='Welcome to the CTF Swiss Army Knife Toolkit!\n Created by Colby Frey test', font=('Rouge', 14))
-
+welcome_label = Label(master, text='Welcome to the CTF Swiss Army Knife Toolkit!\n Created by Colby Frey test', font=('Rouge', 14)) 
 termsOfUseLabel = Label(master, text='By clicking the "I agree" button you agree to the terms of use', font=('Rouge',8))
+testNB = tkrtk.Notebook(master)
+touSheet = tkrtk.Frame(testNB)
+testNB.add(touSheets, text = "tou")
 
 #Entries
 jpEntry = Entry(master, borderwidth=2)
 
 #Buttons
-agreeButon = Button(master, text='Run JohnPollard!', command=buttonClick)
+agreeButton = Button(master, text='Run JohnPollard!', command=buttonClick)
 
 #Grid Assignments
 welcome_label.grid(row = 1, column=0)
 termsOfUseLabel.grid(row=2, column=0)
-agreeButon.grid(row=4)
+agreeButton.grid(row=4)
 jpEntry.grid(row=3)
+testNB.grid()
+
+#List assignments
+labelsList.append(welcome_label)
+labelsList.append(termsOfUseLabel)
+buttonsList.append(agreeButton)
+entriesList.append(jpEntry)
+
 
 #Part
 part_text = StringVar()
